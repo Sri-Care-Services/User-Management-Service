77859ebf2a84646c2051705fa1adbb9fd38d1973
S Manivannan <136373631+suruthiii@users.noreply.github.com>
2024-09-23 11:31:12 +0530

    Update UsersRepo.java

--- src/main/java/com/User_Management_Service/User_Management_Service/Repository/UsersRepo.java
+++ src/main/java/com/User_Management_Service/User_Management_Service/Repository/UsersRepo.java
@@ -1,12 +1,16 @@
 package com.User_Management_Service.User_Management_Service.Repository;
 
 import com.User_Management_Service.User_Management_Service.Entity.Users;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UsersRepo extends JpaRepository<Users, Long> {
     Optional<Users> findByEmail(String email);
+
+    List<Users> findAllByRole(String user);
 }
